ee457ed14b614374a28031428a355744bc14eaea
chuyAguilar <jesusaguilar32265@gmail.com>
2026-02-02 20:25:58 -0600

    quite el contador

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
 // alert("Funciona!")
 
 
-function contador() {
-    let numero = parseInt(localStorage.getItem("contador")) || 0
+// function contador() {
+// let numero = parseInt(localStorage.getItem("contador")) || 0
 
-    const badge = document.querySelector("#badge")
-    const btnAccion = document.querySelector("#accion")
+// const badge = document.querySelector("#badge")
+// const btnAccion = document.querySelector("#accion")
 
 
-    function sumar() {
-        numero = numero + 1
-        localStorage.setItem("contador", numero)
-        render()
-        badge.classList.add("pulse")
-        setTimeout(() => {
-            badge.classList.remove("pulse")
-        }, 150);
-    }
+// function sumar() {
+//     numero = numero + 1
+//     localStorage.setItem("contador", numero)
+//     render()
+//     badge.classList.add("pulse")
+//     setTimeout(() => {
+//         badge.classList.remove("pulse")
+//     }, 150);
+// }
 
-    function render() {
-        badge.textContent = numero
-    }
+// function render() {
+//     badge.textContent = numero
+// }
 
-    render()
+// render()
 
-    btnAccion.addEventListener("click", sumar)
-}
+//     btnAccion.addEventListener("click", sumar)
+// }
 
-const cont = contador()
+// const cont = contador()
 
 const btnConoceme = document.querySelector("#btn1")
 
-function goToCards(){
-const sectionCards = document.querySelector("#cards")
+function goToCards() {
+    const sectionCards = document.querySelector("#cards")
 
-sectionCards.scrollIntoView({ behavior: "smooth" })
+    sectionCards.scrollIntoView({ behavior: "smooth" })
 }
 
-btnConoceme.addEventListener("click",goToCards)
+btnConoceme.addEventListener("click", goToCards)
 
 
 const btnSiguiente = document.querySelector("#btnSiguiente")
 
-function goToContacto(){
+function goToContacto() {
     const sectionContacto = document.querySelector("#contacto")
 
-    sectionContacto.scrollIntoView({behavior: "smooth"})
+    sectionContacto.scrollIntoView({ behavior: "smooth" })
 }
 
-btnSiguiente.addEventListener("click",goToContacto)
+btnSiguiente.addEventListener("click", goToContacto)
