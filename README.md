66a58f2fb84e13ace8a84e282cfcdb4bc10829b6
chuyAguilar <jesusaguilar32265@gmail.com>
2026-02-02 20:39:09 -0600

    elimine codigo comentado de js

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Portafolio
 Portafolio personal. Presentación.
+
+// alert("Funciona!")
+
+
+// function contador() {
+// let numero = parseInt(localStorage.getItem("contador")) || 0
+
+// const badge = document.querySelector("#badge")
+// const btnAccion = document.querySelector("#accion")
+
+
+// function sumar() {
+//     numero = numero + 1
+//     localStorage.setItem("contador", numero)
+//     render()
+//     badge.classList.add("pulse")
+//     setTimeout(() => {
+//         badge.classList.remove("pulse")
+//     }, 150);
+// }
+
+// function render() {
+//     badge.textContent = numero
+// }
+
+// render()
+
+//     btnAccion.addEventListener("click", sumar)
+// }
+
+// const cont = contador()
--- main.js
+++ main.js
@@ -1,33 +1,4 @@
-// alert("Funciona!")
 
-
-// function contador() {
-// let numero = parseInt(localStorage.getItem("contador")) || 0
-
-// const badge = document.querySelector("#badge")
-// const btnAccion = document.querySelector("#accion")
-
-
-// function sumar() {
-//     numero = numero + 1
-//     localStorage.setItem("contador", numero)
-//     render()
-//     badge.classList.add("pulse")
-//     setTimeout(() => {
-//         badge.classList.remove("pulse")
-//     }, 150);
-// }
-
-// function render() {
-//     badge.textContent = numero
-// }
-
-// render()
-
-//     btnAccion.addEventListener("click", sumar)
-// }
-
-// const cont = contador()
 
 const btnConoceme = document.querySelector("#btn1")
 
