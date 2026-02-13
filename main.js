1ea0a41fd9188f5bef2bffc3093a60e7032d3bf2
chuyAguilar <jesusaguilar32265@gmail.com>
2026-02-13 14:12:45 -0600

    agregue proyectos

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 
 
 const btnConoceme = document.querySelector("#btn1")
-
 function goToCards() {
     const sectionCards = document.querySelector("#cards")
 
     sectionCards.scrollIntoView({ behavior: "smooth" })
 }
-
 btnConoceme.addEventListener("click", goToCards)
 
 
 const btnSiguiente = document.querySelector("#btnSiguiente")
-
 function goToContacto() {
     const sectionContacto = document.querySelector("#contacto")
 
     sectionContacto.scrollIntoView({ behavior: "smooth" })
 }
-
 btnSiguiente.addEventListener("click", goToContacto)
+
+const btnProyectos = document.querySelector("#btnProyectos");
+function goToProyectos() {
+    const sectionProyectos = document.querySelector("#proyectos")
+
+    sectionProyectos.scrollIntoView({ behavior: "smooth" })
+}
+btnProyectos.addEventListener("click", goToProyectos);
